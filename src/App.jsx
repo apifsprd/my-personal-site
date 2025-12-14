878ad2496948716bda413cfb5ef812b57f6b78d6
Apif Supriadi <afs.supriadi@gmail.com>
2025-12-14 23:59:59 +0700

    feat: enhance layout and header structure, update footer year dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { Instagram, Linkedin } from "lucide-react";
+import { ArrowUpRight, Instagram, Linkedin } from "lucide-react";
 import "./App.css";
 import { Link } from "react-router";
-import projects from "./api/projects.json";
-import quote from "./api/qoute.json";
 import { useEffect, useState } from "react";
 import moment from "moment";
 import Layout from "./Layout";
@@ -67,61 +65,50 @@ function App() {
   return (
     <Layout lang={lang} onSetLang={setLang}>
       {/* Header */}
-      <div className="w-full h-auto flex flex-col-reverse justify-center items-center gap-4 md:flex-row">
-        <div className="w-full flex flex-col gap-4 justify-center items-center md:justify-start md:items-start">
-          <span className="font-inter font-semibold text-black text-xl lg:text-2xl">
-            {lang === "id" ? headerContent.id.title : headerContent.en.title}
-          </span>
-          <span className="font-inter font-normal text-black text-base text-center md:text-left lg:text-lg">
+      <section className="w-full h-auto flex flex-col justify-start items-start gap-4 md:flex-col">
+        <div>
+          <h1 className="text-xl sm:text-2xl font-bold text-slate-900">
             {lang === "id"
-              ? headerContent.id.description
-              : headerContent.en.description}{" "}
-            <a
-              href={headerContent.currentWork.link}
-              target="_blank"
-              className="font-inter bg-yellow-100 text-yellow-600 text-base hover:underline hover:underline-offset-2 lg:text-lg"
-            >
-              {headerContent.currentWork.company}
-            </a>
-          </span>
-          <div className="flex flex-row justify-start items-center gap-2">
-            <p className="font-inter font-normal text-black text-base text-center lg:text-left">
-              {lang === "id" ? headerContent.id.bio : headerContent.en.bio}
-            </p>
-          </div>
-          <div className="flex flex-row justify-center items-center gap-2">
-            <a
-              href="https://linkedin.com/in/apifsprd"
-              className="w-10 h-10 flex justify-center items-center rounded-full bg-slate-100 hover:bg-slate-50"
-            >
-              <Linkedin size={18} color="#0284C7" />
-            </a>
-            <a
-              href="https://instagram.com/apifsprd"
-              className="w-10 h-10 flex justify-center items-center rounded-full bg-slate-100 hover:bg-slate-50"
-            >
-              <Instagram size={18} color="#0284C7" />
-            </a>
-          </div>
+              ? "Catatan diluar jam kerja"
+              : "Notes outside working hours"}
+          </h1>
         </div>
-        <div className="w-full flex justify-center items-center">
-          <img
-            src="/images/profile/1x1.png"
-            alt="Apif Supriadi"
-            className="w-32 h-32 rounded-full object-cover lg:w-48 lg:h-48 "
-          />
+        <div>
+          <p className="text-base text-slate-800">
+            {lang === "id"
+              ? "Membahas Sepakbola, Humaniora, dan Koding"
+              : "Discussing Football, Humanities, and Code"}
+          </p>
         </div>
-      </div>
+        <div className="flex flex-row gap-4">
+          <a
+            href="https://www.instagram.com/apifsprd"
+            target="_blank"
+            className="px-3 py-1 flex justify-center items-center gap-1 bg-slate-100 rounded-lg hover:scale-105 transition duration-300 ease-in-out"
+          >
+            <p className="text-sm text-slate-900">Instagram</p>
+            <ArrowUpRight size={18} className="text-slate-900" />
+          </a>
+          <a
+            href="https://apifsprd-portfolio.vercel.app/"
+            target="_blank"
+            className="px-3 py-1 flex justify-center items-center gap-1 bg-slate-100 rounded-lg hover:scale-105 transition duration-300 ease-in-out"
+          >
+            <p className="text-sm text-slate-900">Portfolio</p>
+            <ArrowUpRight size={18} className="text-slate-900" />
+          </a>
+        </div>
+      </section>
 
       <div className="flex flex-col gap-4 bg-slate-100 p-4 rounded-xl">
         <span className="font-inter font-semibold text-black text-sm mb-2 md:text-base">
-          {lang === "id" ? "Artikel Terbaru" : "Latest Post"}
+          {lang === "id" ? "Artikel" : "Post"}
         </span>
         {posts.map((post, index) => (
           <Link
             to={`/post/${post.slug}`}
             key={index}
-            className="flex flex-col-reverse justify-between items-start gap-4 bg-white rounded-lg p-2 hover:ring-4 hover:ring-sky-200 md:flex-row cursor-pointer lg:p-4"
+            className="flex flex-col-reverse justify-between items-start gap-4 bg-white rounded-lg p-2 transition ease-in duration-300 hover:bg-sky-100 hover:-translate-y-1 md:flex-row cursor-pointer lg:p-4"
           >
             <div className="flex flex-col gap-1">
               <span className="text-base font-inter font-semibold text-black lg:text-lg">
@@ -142,37 +129,6 @@ function App() {
               className="w-full h-32 md:w-32 lg:w-44 lg:h-32 rounded-lg object-cover"
             /> */}
           </Link>
-        ))}
-      </div>
-      {/* Project */}
-      <div className="flex flex-col gap-4 bg-slate-100 p-4 rounded-xl">
-        <span className="font-inter font-semibold text-black text-base mb-2">
-          {lang === "id" ? "Proyek Terbaru" : "Latest Project"}
-        </span>
-        {projects.map((project, index) => (
-          <a
-            href={project.link}
-            key={index}
-            target="_blank"
-            className="flex flex-col justify-start items-start gap-2 bg-white rounded-lg p-2 hover:ring-4 hover:ring-sky-200 md:flex-row cursor-pointer"
-          >
-            <img
-              src={project.image}
-              alt="project_image"
-              className="w-14 h-14 lg:w-24 lg:h-24 rounded-lg object-cover"
-            />
-            <div className="flex flex-col gap-2">
-              <span className="text-sm font-inter font-semibold text-black lg:text-lg">
-                {project.name}
-              </span>
-              <span className="text-xs font-inter font-normal text-black lg:text-base">
-                {lang === "id" ? project.description : project.description_en}
-              </span>
-              <span className="text-xs font-inter font-normal text-slate-600 lg:text-sm">
-                <span className="text-sky-600"> {project.stack[0]}</span>
-              </span>
-            </div>
-          </a>
         ))}
       </div>
     </Layout>
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,28 +4,13 @@ import React from "react";
 function Layout({ lang, onSetLang, children }) {
   return (
     <>
-      <div className="w-full min-h-screen flex flex-col gap-8 px-4 py-4 sm:px-[20vw] lg:px-[30vw] bg-white">
-        <div className="w-full h-auto flex flex-row justify-between items-center">
-          <div className="flex flex-row justify-start items-center gap-2">
-            <img
-              src="https://flagcdn.com/w20/id.png"
-              srcset="https://flagcdn.com/w40/id.png 2x"
-              width="24"
-              alt="Indonesia Flag"
-              className="border border-yellow-500"
-            />
-            <img
-              src="https://flagcdn.com/w20/ps.png"
-              srcset="https://flagcdn.com/w40/ps.png 2x"
-              width="30"
-              alt="Indonesia Flag"
-              className="border border-yellow-500"
-            />
-            <a
-              href="/"
-              className="font-inter font-semibold text-black text-sm md:text-base lg:text-lg hover:bg-yellow-100 transition ease-linear duration-300"
-            >
-              @apifsprd
+      <main className="w-full min-h-screen flex flex-col gap-8 px-4 py-4 sm:px-[20vw] lg:px-[30vw] bg-white">
+        <section className="w-full h-auto flex flex-row justify-between items-center">
+          <div className="flex flex-row justify-start items-center gap-0">
+            <a href="/">
+              <h1 className="font-inter font-semibold text-black text-lg md:text-lg lg:text-2xl hover:bg-yellow-100 transition ease-linear duration-300 ml-0 italic">
+                Apif Supriadi
+              </h1>
             </a>
           </div>
           <div className="flex items-center gap-2">
@@ -39,12 +24,12 @@ function Layout({ lang, onSetLang, children }) {
               </span>
             </button>
           </div>
-        </div>
+        </section>
         {children}
-      </div>
+      </main>
       <footer className="flex justify-center items-center p-4 mt-14 bg-slate-100">
         <span className="text-xs font-inter font-normal text-slate-500 lg:text-sm">
-          &copy; 2025 Apif Supriadi
+          &copy; {new Date().getFullYear()} Apif Supriadi
         </span>
       </footer>
     </>
